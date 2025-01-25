7040c59f18fc2a4aedc8f87aa403820cc29734e9
NovaTheThird <nicholasalexanderbrennan@gmail.com>
2025-01-25 13:52:08 -0800

    Updated player and pause menu + levels

--- ggj25/scripts/scr_Player.gd
+++ ggj25/scripts/scr_Player.gd
@@ -1,29 +1,30 @@
 extends Node3D
 
-@onready var pause = $Pause
+@onready var pause = get_node("../../../PauseMenu/Pause")  # Place PauseMenu as seen in lvl_NickTest
 @onready var player = $CharacterBody3D
 var isPaused = false
+var isFullscreen = false
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	process_mode = Node.PROCESS_MODE_ALWAYS
-	$Pause/Control/Continue.connect("pressed", _continue)
-	$Pause/Control/Exit.connect("pressed", _quit)
-	
+	pause.visible = false
+	pause.get_node("Control/Continue").connect("pressed", _continue)
+	pause.get_node("Control/Exit").connect("pressed", _quit)
+
 func _continue():
 	if isPaused == true:
 		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 		pause.visible = false
 		get_tree().paused = false
 		isPaused = false
-		
+
 func _quit():
 	get_tree().quit()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_just_released("ui_cancel"):
+	if Input.is_action_just_pressed("ui_cancel"):
 		if isPaused == false:
 			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 			pause.visible = true
@@ -33,4 +34,3 @@ func _process(delta: float) -> void:
 			pause.visible = false
 			get_tree().paused = false
 		isPaused = !isPaused
-		
