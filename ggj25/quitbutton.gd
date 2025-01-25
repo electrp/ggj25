2086bd4485d3ffaefa5c5cc0e2e7ccb71132f5d7
JamesHardy141 <126114251+JamesHardy141@users.noreply.github.com>
2025-01-25 02:54:46 -0800

    Added main menu
    main menu fool

---
+++ ggj25/quitbutton.gd
@@ -0,0 +1,18 @@
+extends Sprite2D
+
+var clicked = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if clicked:
+		get_tree().quit()
+
+func _input(event) -> void:
+	if event is InputEventMouseButton and event.is_pressed() and event.button_index == MOUSE_BUTTON_LEFT:
+		if get_rect().has_point(to_local(event.position)):
+			clicked = true
