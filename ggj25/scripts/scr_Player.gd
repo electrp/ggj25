7812969d72bf6d529a9958375ca6eff9708feb48
NovaTheThird <nicholasalexanderbrennan@gmail.com>
2025-01-25 15:28:28 -0800

    viewmodel no longer clips

--- ggj25/scripts/scr_Player.gd
+++ ggj25/scripts/scr_Player.gd
@@ -4,6 +4,9 @@ extends Node3D
 @onready var player = $CharacterBody3D
 @onready var fan_obj = $CharacterBody3D/PortableFan/Fan
 
+@onready var camera = $CharacterBody3D/Head/Camera3D
+@onready var viewmodel_camera = $CharacterBody3D/Head/Camera3D/SubViewportContainer/SubViewport/ViewmodelCam
+
 var isPaused = false
 var isFullscreen = false
 
@@ -26,6 +29,8 @@ func _quit():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	viewmodel_camera.global_transform = camera.global_transform
+	
 	fan_obj.activate = Input.is_action_pressed("Activate")
 	
 	if Input.is_action_just_pressed("ui_cancel"):
