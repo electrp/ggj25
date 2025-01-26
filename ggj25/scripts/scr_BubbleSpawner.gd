74eead4bcf2fcd1738001c69b2c9ed1ef0bd8b46
electrp <william.bender@digipen.edu>
2025-01-26 14:00:11 -0800

    All one room

--- ggj25/scripts/scr_BubbleSpawner.gd
+++ ggj25/scripts/scr_BubbleSpawner.gd
@@ -2,6 +2,7 @@ extends CSGCylinder3D
 
 @export var bubble_scene: PackedScene
 @export var spawned_bubble: RigidBody3D
+@export var enabled: bool = true
 
 var canShoot = true
 
@@ -12,7 +13,7 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if (spawned_bubble == null) and canShoot == true:
+	if (spawned_bubble == null) and canShoot == true and enabled:
 		spawned_bubble = bubble_scene.instantiate();
 		add_child(spawned_bubble);
 		spawned_bubble.position = $Marker3D.position
@@ -21,3 +22,10 @@ func _process(delta: float) -> void:
 
 func _on_body_entered(body: Node):
 	canShoot = true
+	
+	
+
+
+func enable_bubble() -> void:
+	enabled = true
+	pass # Replace with function body.
