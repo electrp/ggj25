936211c64ad4f16934f46e526a803212555e6d68
sangbeom.kim <sangbeom.kim@digipen.edu>
2025-01-25 18:40:25 -0800

    making bubbles look better

--- ggj25/scripts/scr_BubbleSpawner.gd
+++ ggj25/scripts/scr_BubbleSpawner.gd
@@ -3,15 +3,21 @@ extends CSGCylinder3D
 @export var bubble_scene: PackedScene
 @export var spawned_bubble: RigidBody3D
 
+var canShoot = true
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if(spawned_bubble == null):
+	if (spawned_bubble == null) and canShoot == true:
 		spawned_bubble = bubble_scene.instantiate();
 		add_child(spawned_bubble);
 		spawned_bubble.position = $Marker3D.position
+		spawned_bubble.apply_impulse(($Marker3D.global_position - global_position) * 50)
 	pass
+
+func _on_body_entered(body: Node):
+	canShoot = true
---
+++ ggj25/scripts/scr_SpawnerRange.gd
@@ -0,0 +1,17 @@
+extends Area3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	connect("body_entered", Callable(self, "_on_body_entered"))
+	connect("body_exited" , Callable(self, "_on_body_exited"))
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_body_entered(body: Node) -> void:
+	get_parent().canShoot = false
+
+func _on_body_exited(body: Node) -> void:
+	get_parent().canShoot = true
