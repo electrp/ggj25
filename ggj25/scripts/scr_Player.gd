8702b3630936e296dc25660f65ab1b2bee5a05ab
NovaTheThird <nicholasalexanderbrennan@gmail.com>
2025-01-25 16:38:03 -0800

    revert viewmodel

--- ggj25/scripts/scr_Player.gd
+++ ggj25/scripts/scr_Player.gd
@@ -13,6 +13,4 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	viewmodel_camera.global_transform = camera.global_transform
-	
 	fan_obj.activate = Input.is_action_pressed("Activate")
